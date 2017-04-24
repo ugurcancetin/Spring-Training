f3605515cb1960cf141ce61bc6095dcceb652244
Uğurcan Çetin <ugurcan.cetin@liferay.com>
2017-04-24 17:57:40 +0200

    Dependency Injection Sample

---
+++ 02-Hello-Spring-DI/src/com/ugurcan/spring/training/HelloSpringApp.java
@@ -0,0 +1,23 @@
+package com.ugurcan.spring.training;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		
+		//load the spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve bean from spring container
+		Trainer trainer = context.getBean("myTrainer", Trainer.class);
+		
+		//call methods on the bean
+		System.out.println(trainer.getDailyTraining());
+		
+		//close the context
+		context.close();
+	}
+
+}
