61422b6f2d72b8922fddd2cf6383731926972820
ugurcan.cetin <ugurcan.cetin@liferay.com>
2017-04-30 15:12:36 +0200

    Spring Annotations Autowiring Setter Injection

---
+++ 11-Spring-Demo-Annotations-Autowiring-SetterInjection/src/com/ugurcan/springtraining/AngularTrainer.java
@@ -0,0 +1,39 @@
+package com.ugurcan.springtraining;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AngularTrainer implements Trainer{
+
+	private FortuneService fortuneService;
+	
+	/*	@Autowired
+	public AngularTrainer(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}*/
+	
+	//define constructor with no args 
+	public AngularTrainer() {
+		System.out.println("Angular Training -> Inside of Contructor with no args");
+	}
+
+	//create the setter injection here
+	@Autowired
+	public void setFortuneService(FortuneService thefortuneService) {
+		this.fortuneService = thefortuneService;
+		System.out.println("Angular Training -> Inside of Setter Method-Injection");
+	}
+
+	
+	@Override
+	public String GetDailyTraining() {
+		return "This is new Angular Training : Welcome to Angular Class";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
