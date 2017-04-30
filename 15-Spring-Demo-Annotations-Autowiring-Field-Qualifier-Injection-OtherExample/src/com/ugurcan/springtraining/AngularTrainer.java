82e9d67cecda1d446ec360e7dcf2725b2e1a765a
ugurcan.cetin <ugurcan.cetin@liferay.com>
2017-04-30 22:07:01 +0200

    Other examle with Autowiring-Qualifer Injection

---
+++ 15-Spring-Demo-Annotations-Autowiring-Field-Qualifier-Injection-OtherExample/src/com/ugurcan/springtraining/AngularTrainer.java
@@ -0,0 +1,30 @@
+package com.ugurcan.springtraining;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AngularTrainer implements Trainer{
+
+	//Class name with lowercase beginning
+	@Autowired
+	@Qualifier("randomFortuneService")
+	private FortuneService fortuneService;
+	
+	//define constructor with no args 
+	public AngularTrainer() {
+		System.out.println("Angular Training -> Inside of Contructor with no args");
+	}
+
+	@Override
+	public String GetDailyTraining() {
+		return "This is new Angular Training : Welcome to Angular Class";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
