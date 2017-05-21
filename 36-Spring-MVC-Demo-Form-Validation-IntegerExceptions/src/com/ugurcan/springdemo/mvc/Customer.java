00547977a074e7883a1984d6063248fedeb714e2
Uğurcan Çetin <ugurcan.cetin@liferay.com>
2017-05-21 16:46:19 +0200

    Spring Form Validation Handling with int with property file

---
+++ 36-Spring-MVC-Demo-Form-Validation-IntegerExceptions/src/com/ugurcan/springdemo/mvc/Customer.java
@@ -0,0 +1,71 @@
+
+package com.ugurcan.springdemo.mvc;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class Customer
+{
+
+	private String firstName;
+
+	@NotNull( message = "Required" )
+	@Size( min = 4, max = 25, message = "must be between 4 - 25 characters" )
+	private String lastName;
+
+	@NotNull( message = "Required" )
+	@Min( value = 1, message = "Minumum 1 passing" )
+	@Max( value = 5, message = "Maximum 5 passes" )
+	private Integer freePasses;
+
+	@Pattern(regexp="^[a-zA-z0-9]{5}", message="Not Valid Postal Code")
+	private String postalCode;
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public void setFirstName( String firstName )
+	{
+		this.firstName = firstName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public void setLastName( String lastName )
+	{
+		System.out.println( "| " + lastName + " |" );
+		this.lastName = lastName;
+	}
+
+	public Integer getFreePasses()
+	{
+		return freePasses;
+	}
+
+	public void setFreePasses( Integer freePasses )
+	{
+		this.freePasses = freePasses;
+	}
+
+	
+	public String getPostalCode()
+	{
+		return postalCode;
+	}
+
+	
+	public void setPostalCode( String postalCode )
+	{
+		this.postalCode = postalCode;
+	}
+
+	
+}
