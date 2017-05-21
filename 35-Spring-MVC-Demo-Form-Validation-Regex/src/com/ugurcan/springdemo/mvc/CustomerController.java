9ff1f997cf7e1d4d57c72b16807649542080a753
Uğurcan Çetin <ugurcan.cetin@liferay.com>
2017-05-21 16:21:28 +0200

    Spring Form Validation adding a regex

---
+++ 35-Spring-MVC-Demo-Form-Validation-Regex/src/com/ugurcan/springdemo/mvc/CustomerController.java
@@ -0,0 +1,50 @@
+package com.ugurcan.springdemo.mvc;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	
+	//add an initbinder to trim the whitespaces 
+	//from input
+	@InitBinder
+	public void InitBinder(WebDataBinder dataBinder){
+		
+		StringTrimmerEditor STE = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, STE);
+	}
+	
+	
+	
+	@RequestMapping("/showForm")
+	public String showForm(Model theModel){
+		
+		theModel.addAttribute("customer", new Customer());
+		
+		return "customer-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(
+			@Valid @ModelAttribute("customer") Customer theCustomer, 
+			BindingResult theBindingResult){
+		
+		if (theBindingResult.hasErrors()){
+			return "customer-form";
+		}
+		else { 
+			return "customer-confirmation";
+		}
+	}
+}
