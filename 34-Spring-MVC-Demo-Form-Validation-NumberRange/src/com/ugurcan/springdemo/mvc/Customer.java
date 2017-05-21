cc7bc1a828b53cfdd7175354773f2d38c2a3b9a8
Uğurcan Çetin <ugurcan.cetin@liferay.com>
2017-05-21 16:10:44 +0200

    Spring Form Validation adding a number range

---
+++ 34-Spring-MVC-Demo-Form-Validation-NumberRange/src/com/ugurcan/springdemo/mvc/Customer.java
@@ -0,0 +1,53 @@
+
+package com.ugurcan.springdemo.mvc;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Customer
+{
+
+	private String firstName;
+
+	@NotNull( message = "Required" )
+	@Size( min = 4, max = 25, message = "must be between 4 - 25 characters" )
+	private String lastName;
+
+	@Min( value = 1, message = "Minumum 1 passing" )
+	@Max( value = 5, message = "Maximum 5 passes" )
+	private int freePasses;
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public void setFirstName( String firstName )
+	{
+		this.firstName = firstName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public void setLastName( String lastName )
+	{
+		System.out.println( "| " + lastName + " |" );
+		this.lastName = lastName;
+	}
+
+	public int getFreePasses()
+	{
+		return freePasses;
+	}
+
+	public void setFreePasses( int freePasses )
+	{
+		this.freePasses = freePasses;
+	}
+
+}
