e031b7e2b7f10d66f03e47f6441537a0e6a4b20e
Uğurcan Çetin <ugurcan.cetin@liferay.com>
2017-05-25 16:15:29 +0200

    Hibernate Annoatations and MySql strategies

---
+++ 41-Hibernate-Tutorial-AnnotationsBasics2/src/com/ugurcan/jdbc/TestJdbc.java
@@ -0,0 +1,28 @@
+package com.ugurcan.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "root";
+		String pass = "root";
+		
+		try {
+			System.out.println("Connecting to database ... " + jdbcUrl);
+			
+			Connection myConn=
+				 DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connectiong Succesful!!!");
+			
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
