ca6bd29122db44cc2622472b8d73c5f59d8c364f
Uğurcan Çetin <ugurcan.cetin@liferay.com>
2017-05-27 17:59:19 +0200

    updating the wrong class

--- 43-Hibernate-Tutorial-Query-HQL/src/com/ugurcan/hibernate/demo/ReadStudentDemo.java
+++ 43-Hibernate-Tutorial-Query-HQL/src/com/ugurcan/hibernate/demo/ReadStudentDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.ugurcan.hibernate.demo.entity.Student;
 
-public class ReadStudentDemo {
+public class ReadStudentDemo{
 
 	public static void main(String[] args) {
 
@@ -22,21 +22,38 @@ public class ReadStudentDemo {
 		try {
 			//create Student object
 			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Paul", "WALL", "paul@liferay.com");
+			Student tempStudent = new Student("Ali", "VELI", "aveli@liferay.com");
 			
 			//start a transaction
 			session.beginTransaction();
 			
 			//save the student object
 			System.out.println("Saving the student...");
+			System.out.println( tempStudent );
 			session.save(tempStudent);
 			
 			//commit transaction
 			session.getTransaction().commit();
-			
-			
+
 			System.out.println("Student saved succesfully!");
-			
+			System.out.println( "--------------------------------------------------------" );
+			System.out.println( "Generated Id for this student = " + tempStudent.getId() );
+			System.out.println( "--------------------------------------------------------" );
+			//now get a new sessiong and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+
+			//retrieve the student back based on studentId
+			System.out.println( " ...Getting student with id " +tempStudent.getId() );
+			Student myStudent = session.get( Student.class, tempStudent.getId() );
+			System.out.println( "--------------------------------------------------------" );
+
+			System.out.println( "Student Informations : " + myStudent );
+
+			//commit the Transaction
+			session.getTransaction().commit();
+			System.out.println( "--------------------------------------------------------" );
+			System.out.println( "SUCCESSFUL" );
 		} finally {
 			factory.close();
 		}
