5b1430f2ec52b605d953533893e1807389cb252c
ugurcan.cetin <ugurcan.cetin@liferay.com>
2017-06-11 01:20:49 +0200

    Add Customer

---
+++ 51-SpringMVC-CRUD-Hibernate-AddCustomer/src/com/ugurcan/springdemo/DAO/CustomerDAOImpl.java
@@ -0,0 +1,49 @@
+package com.ugurcan.springdemo.DAO;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ugurcan.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	//need to inject te session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	public List<Customer> getCustomers() {
+		
+		//get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		//create a query...sort by last name
+		Query<Customer> theQuery = 
+				currentSession.createQuery("from Customer order by lastName", Customer.class);
+		
+		
+		//execute query ad get result list
+		List<Customer> customers = theQuery.getResultList();
+		
+		//return the results
+		
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save the customer
+		currentSession.save(theCustomer);
+	}
+
+}
---
+++ 51-SpringMVC-CRUD-Hibernate-AddCustomer/src/com/ugurcan/springdemo/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.ugurcan.springdemo.service;
+
+import java.util.List;
+
+import com.ugurcan.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+}
