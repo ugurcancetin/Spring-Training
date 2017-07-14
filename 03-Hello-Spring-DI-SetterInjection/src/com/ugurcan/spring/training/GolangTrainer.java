e4f50dd46c6e091effd49240fb4b6d038aa69838
Uğurcan Çetin <ugurcan.cetin@liferay.com>
2017-07-14 23:24:01 +0200

    SF

--- 03-Hello-Spring-DI-SetterInjection/src/com/ugurcan/spring/training/GolangTrainer.java
+++ 03-Hello-Spring-DI-SetterInjection/src/com/ugurcan/spring/training/GolangTrainer.java
@@ -1,24 +1,17 @@
 package com.ugurcan.spring.training;
 
 public class GolangTrainer implements Trainer {
-	
+
 	private FortuneService fortuneService;
 
-	
-	
-	
 	public GolangTrainer() {
 		System.out.println("Inside of no-arg Constructor of GolangTrainer"); 
 	}
 
-	
-	
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 		System.out.println("Inside of setter Method of GolangTrainer"); 
 	}
-
-
 
 	@Override
 	public String getDailyTraining() {
