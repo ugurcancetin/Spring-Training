c284d89028c1205387587ff72cf9ba35c0b95f40
Uğurcan Çetin <ugurcan.cetin@liferay.com>
2017-07-14 22:55:20 +0200

    Some more compatibility to get the difference for dependency injection

--- 02-Hello-Spring-DI/src/com/ugurcan/spring/training/HelloSpringApp.java
+++ 02-Hello-Spring-DI/src/com/ugurcan/spring/training/HelloSpringApp.java
@@ -5,20 +5,24 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class HelloSpringApp {
 
 	public static void main(String[] args) {
-		
+
 		//load the spring configuration file
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+
 		//retrieve bean from spring container
 		Trainer trainer = context.getBean("myTrainer", Trainer.class);
+
+		FortuneService fortuneService = context.getBean("myFortune", FortuneService.class);
+		System.out.println(fortuneService.getFortune());
+		
 		
 		//call methods on the bean
 		System.out.println(trainer.getDailyTraining());
-		
+
 		//let's call our new methods for fortunes
 		System.out.println(trainer.getDailyFortune());
-		
+
 		//close the context
 		context.close();
 	}
